d02798a430f678a219dd17b76f5c75109ef42e19
Gazi Akter <gaziakterdev@gmail.com>
2024-04-24 07:54:37 +0600

    google font

--- inc/common/scripts.php
+++ inc/common/scripts.php
@@ -5,6 +5,7 @@
  */
 function venue_style_scripts(){
 
+    wp_enqueue_style('venue-fonts', venue_fonts_url(), array(), '1.0.0', 'all');
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '5.1.3', 'all');
     wp_enqueue_style('bootstrap-them', get_template_directory_uri() . '/assets/css/bootstrap-theme.min.css', array(), '2.0.7', 'all');
     wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/css/fontAwesome.css', array(), '1.0.0', 'all');
@@ -21,3 +22,19 @@ function venue_style_scripts(){
 }
 
 add_action('wp_enqueue_scripts', 'venue_style_scripts');
+
+/*
+Register Fonts
+ */
+function venue_fonts_url(){
+    $font_url = '';
+
+    /*
+    Translators: If there are characters in your language that are not supported
+    by chosen font(s), translate this to 'off'. Do not translate into your own language.
+     */
+    if ('off' !== _x('on', 'Google font: on or off', 'venue')) {
+        $font_url = 'https://fonts.googleapis.com/css?' . urlencode('family=Raleway:100,200,300,400,500,600,700,800,900');
+    }
+    return $font_url;
+}
